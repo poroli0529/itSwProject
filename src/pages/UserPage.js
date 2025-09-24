7a00b9b246a50d43aa1829da3a3015f9a3c1377b
poroli0529 <kafaprl@gmail.com>
2025-09-24 16:04:19 +0900

    week05

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,9 +1,51 @@
 // pages/UserPage.js
 const UserPage = () => {
+
+    const mockdata =  [
+        {
+          name: "이승종",
+          departure: "컴퓨터정보과",
+          stdno: "202244083",
+        },
+        {
+          name: "이승종",
+          departure: "컴퓨터정보과",
+          stdno: "202244084",
+        }
+        
+    ];
+      
+       
+  
   return (
     <div>
-      <h1>내 정보</h1>
-      <p>여기에 유저 프로필, 내가 쓴 글, 내가 등록한 물건 등을 보여줍니다.</p>
+      <div>
+      
+      <h1>사용자 페이지</h1>
+      <ul>
+        {mockdata
+        .filter(user => user.stdno === "202244083")
+        .map((user,index)=>(
+            
+          <li key={index}>
+            이름: {user.name} <br/>학과: {user.departure}<br/> 학번: {user.stdno}<br/>
+          </li>
+        ))}
+      </ul>
+     
+    </div>
+    
+   
+
+
+      <h4>내가 쓴 글</h4>
+
+        날짜 + 제목 구성으로 데이터베이스에서 글가져오기
+
+      <h4>내가 등록한 물건</h4>
+      
+      등록날짜 + 사진 + 제목 구성으로 데이터베이스에서 상품 정보 가져오기
+
     </div>
   );
 };
